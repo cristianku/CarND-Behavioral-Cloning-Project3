18295c17c89638682d6f20f0c98b185e40db0fd9
cristianku <c.zantedeschi@gmail.com>
2017-10-17 23:49:21 +0200

    udacity

--- drive.py
+++ drive.py
@@ -62,7 +62,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.resize(image_array,(64,64 ))
+        image_array = cv2.resize(image_array,(128,128 ))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
